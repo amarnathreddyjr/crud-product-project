e9c1b94cd7769602a118284d7707d9bc23ccedf4
amarnathreddyjr <amarnathreddyjr@gmail.com>
2025-05-29 11:46:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ let app=express();
 let baseUrl='/api/products';
 app.use(express.json());
 let productController=require("./controllers/productController");
-// let productModel=require('./models/productSchema');///////////
+// let productModel=require('./models/productSchema');
 let mongoose=require("mongoose");
 mongoose.connect('mongodb://localhost:27017/crud-prooduct-project-db').then(result=>{
     console.log('connected database crud-prooduct-project-db successfully');
 });
-// app.post(`${baseUrl}/create`,async(req,res)=>{//
+// app.post(`${baseUrl}/create`,async(req,res)=>{
 //     let bodyData=req.body;//
-//     console.log('the bodyData reseved for create operation',bodyData);//
-//     let product=new productModel({//
-//         name:bodyData.name,//
+//     console.log('the bodyData reseved for create operation',bodyData);
+//     let product=new productModel({
+//         name:bodyData.name,
 //         price:bodyData.price,//
-//         description:bodyData.description,//
-//         category:bodyData.category////
-//     });//
-//    let response=await product.save();//
-//    res.status(201).send(response);//
-// });//
+//         description:bodyData.description,
+//         category:bodyData.category
+//     });
+//    let response=await product.save();
+//    res.status(201).send(response);
+// });
 app.post(`${baseUrl}/create`,productController.ceate);
 app.get(`${baseUrl}/list`,productController.list);
 app.get(`${baseUrl}/Details/:id`,productController.Details);
