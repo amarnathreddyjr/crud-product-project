1908e902562f6096c4df5300875f5f87f3226790
amarnathreddyjr <amarnathreddyjr@gmail.com>
2025-05-29 10:50:25 +0530

    Add files via upload

---
+++ controllers/productController.js
@@ -0,0 +1,54 @@
+let productModel=require('../models/productSchema');
+let productController={
+  ceate: async(req,res)=>{
+    let bodyData=req.body;
+    console.log('the bodyData reseved for create operation',bodyData);
+    let product=new productModel({
+        name:bodyData.name,
+        price:bodyData.price,
+        description:bodyData.description,
+        category:bodyData.category
+    });
+   let response=await product.save();
+   res.status(201).send(response);
+},
+list: async(req,res)=>{
+    let response=await productModel.find();
+   res.status(200).send(response);
+
+},
+Details:async(req,res)=>{
+    let reqId=req.params.id;
+    let response=await productModel.findOne({_id:reqId});
+    res.status(200).send(response);
+},
+Update:async(req,res)=>{
+  let reqId=req.params.id;
+  let reqBody=req.body;
+  let response=await productModel.updateOne(
+    {_id:reqId},
+    {$set:reqBody}
+  );
+res.status(200).send(response);
+},
+delete:async(req,res)=>{
+    let reqId=req.params.id;
+    let response=await productModel.deleteOne({_id:reqId});
+    res.status(200).send(response);
+},
+
+};
+module.exports=productController;
+
+
+// update:async(req,res)=>{
+//                 let reqId=req.params.id;
+//                 let reqBody=req.body;
+//                 let response=await productModel.updateOne(
+//                     {_id:reqId},
+//                     {$set:reqBody},
+
+//                 );
+//                 res.status(CONSTANT.STATUS_CODE.SUCESS).send(response);
+            
+//                 },
